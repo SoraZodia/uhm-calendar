93d3afa16c6c50375d0343aaf52f3f0d9f12ce46
SoraZodia <sorazodia@users.noreply.github.com>
2017-03-21 00:03:43 -1000

    created edit page, a js for the database

--- app/imports/api/stuff/index.js
+++ app/imports/api/stuff/index.js
@@ -1,1 +1,2 @@
+import './calendar.js';
 import './stuff.js';
--- app/imports/ui/pages/index.js
+++ app/imports/ui/pages/index.js
@@ -5,3 +5,6 @@ import './login-page';
 
 import './add-page.html';
 import './add-page.js';
+
+import './edit-page.html';
+import './edit-page.js';
